e51c0e4b1fe38c19d1e9085752ae6693eaa1b72a
Dmitry Murzin <diraria@yandex.ru>
2018-11-24 16:21:35 +0300

    create basic pages

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,27 +1,20 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Random from './views/Random.vue';
+import About from './views/About.vue';
+import Donate from './views/Donate.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
-    }
-  ]
-})
+    { path: '/', component: Home },
+    { path: '/random', component: Random },
+    { path: '/about', component: About },
+    { path: '/donate', component: Donate },
+    { path: '*', redirect: '/' },
+  ],
+});
