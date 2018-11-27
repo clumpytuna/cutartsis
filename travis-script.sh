2d7cfaefaf9657ee162ee539a67ed06fb319d16c
Dmitry Murzin <diraria@yandex.ru>
2018-11-27 14:24:11 +0300

    Try to move assets in /static directory

--- travis-script.sh
+++ travis-script.sh
@@ -20,6 +20,6 @@ cd ~/deploy
 
 # перезапуск docker контейнеров на сервере
 sudo docker stop cutartsis-frontend || true
-sudo docker run -p 80:80 --name cutartsis-frontend -d cutartsis-frontend
+sudo docker run -p 80:80 --rm --name cutartsis-frontend -d cutartsis-frontend
 ENDSSH
 echo "Deploy complete!"
