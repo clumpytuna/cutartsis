29f12547850ce3633d9504e3ed2b035bfd2d50e2
Clumpy Tuna <anuhinm@mail.ru>
2019-11-01 14:34:43 +0300

    Update CI

--- travis-script.sh
+++ travis-script.sh
@@ -4,10 +4,15 @@ set -x
 
 REMOTE=cutartsis@207.154.229.41
 
-sshpass -p $PASS ssh  -o StrictHostKeyChecking=no ${REMOTE}
+sshpass -p $PASS ssh  -o StrictHostKeyChecking=no ${REMOTE} << ENDSSH
+
+set -e
+set -x
 
 # To generate $GITHUB_TOKEN use our github account (???) and page https://github.com/settings/tokens
 # You can find it credintals here ???
+
+
 rm -rf ~/deploy
 git clone --depth 1 "https://$GITHUB_TOKEN@github.com/clumpytuna/cutartsis.git" ~/deploy
 
